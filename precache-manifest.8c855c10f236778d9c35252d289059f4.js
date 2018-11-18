e7d9aa181a08921782013fcf882f1f854b5b400a
ali-sasani <36729660+ali-sasani@users.noreply.github.com>
2018-11-18 15:06:04 +0330

    Updates

---
+++ precache-manifest.8c855c10f236778d9c35252d289059f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5f5139b78fafdf93bd1",
+    "url": "/calculator/static/js/runtime~main.b5f5139b.js"
+  },
+  {
+    "revision": "4744b5b4595fe13c051b",
+    "url": "/calculator/static/js/main.4744b5b4.chunk.js"
+  },
+  {
+    "revision": "82d8bd4405824813b9ee",
+    "url": "/calculator/static/js/1.82d8bd44.chunk.js"
+  },
+  {
+    "revision": "4744b5b4595fe13c051b",
+    "url": "/calculator/static/css/main.626ee314.chunk.css"
+  },
+  {
+    "revision": "cae8e2ab1f0670ad7469a8542bc5feec",
+    "url": "/calculator/index.html"
+  }
+];
